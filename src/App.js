bf3175eab3c0602e32cfb8c986a8d0bb7feb3a68
unknown <nicolas.ferreiraxn10@gmail.com>
2020-12-07 19:30:09 -0300

    Desafio concluido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ projects, setProjects ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data)
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Projeto: ${Date.now()}`,
+      owner: "Nicolas Ferreira"
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    
+    setProjects(projects.filter(project => project.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects.map(project => (<li key={project.id}>{project.title}<button onClick={() => handleRemoveRepository(project.id)}>Remover</button></li>))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
